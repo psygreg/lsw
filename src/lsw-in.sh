4809e6535b8f3ea840c3cac40e7bf96ed0102e1e
Vinicius Resende dos Santos <vini_handball@hotmail.com>
2025-08-20 11:48:26 -0300

    Fix: corrigindo erro de digitação.

--- src/lsw-in.sh
+++ src/lsw-in.sh
@@ -34,7 +34,7 @@ depcheck () {
         else
             _packages+=("freerdp3-x11")
         fi
-        insta ca-certificates curl
+        install ca-certificates curl
         sudo install -m 0755 -d /etc/apt/keyrings
         sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
         sudo chmod a+r /etc/apt/keyrings/docker.asc
